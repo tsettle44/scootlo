dc0c216d7f96514bc575cdba41f9e35b10c5d7dc
tsettle44 <tsettle44@gmail.com>
2018-11-16 11:36:02 -0500

    laptop commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>scootlo</Text>
+        <Text>LAPTOP</Text>
       </View>
     );
   }
